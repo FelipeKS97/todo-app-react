6503a195a3b56728f122810c25b9ae7827e3e0c5
Felipe Cavalcanti <felipecavalcanti100@gmail.com>
2017-08-23 16:11:14 -0300

    API problem solved, CORS only left to configure

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -4,6 +4,7 @@ module.exports = function (server) {
     
     // API Routes
     const router = express.Router();
+    server.use('/api', router)
 
     //TODO Routes
     const todoService = require('../api/todo/todoService')
