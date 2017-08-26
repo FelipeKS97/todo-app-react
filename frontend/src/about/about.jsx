3f240e7969f819287ef28155e4e52c3ff85b094d
Felipe Cavalcanti <felipecavalcanti100@gmail.com>
2017-08-26 10:41:32 -0300

    Adding react-router addtion to the SPA, Started todoForm and todoList components (And aditional helper components)

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import PageHeader from '../template/pageHeader'
 
 export default props => (
         <div>
-            <h1>About</h1>
+            <PageHeader name='About' small='Me' />
+            <center>
+                <img src="https://s-media-cache-ak0.pinimg.com/originals/19/2c/d9/192cd928c4a604cca04b1913550557dd.jpg" />
+            </center>
         </div>
 )
---
+++ frontend/src/todo/todoList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default props => (
+        <header className='page-header'>
+            <h3>todoList</h3>
+        </header>
+)
